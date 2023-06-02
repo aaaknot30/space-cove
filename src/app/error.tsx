b43fe7ef7cddd599c0f6f37809eb7ca9a78e7a1d
Kyle Larson <aaaknot30@yahoo.com>
2023-06-01 22:32:22 -0600

    cc

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -16,15 +16,8 @@ export default function Error({
  
   return (
     <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+      <h1>Ooops! Looks like the search parameter was not found. Try starting over.</h1>
+      
     </div>
   );
 }
