6d50615fdcfb9d6121b6fc7e1bb0075a91e4f2e7
Kyle Larson <aaaknot30@yahoo.com>
2023-06-01 22:41:34 -0600

    cc

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 'use client'; // Error components must be Client Components
  
 import { useEffect } from 'react';
+import Link from 'next/link';
  
 export default function Error({
   error,
@@ -16,7 +17,16 @@ export default function Error({
  
   return (
     <div>
-      <h1>Ooops! Looks like the search parameter was not found. Try starting over.</h1>
+      <br />
+      <br />
+      <h2>Ooops! Looks like the search parameter was not found. Try starting over.</h2>
+      <br /><Link href="/">Home</Link><br />
+      <Link href="/apod">Daily Pic</Link><br />
+      <Link href="/rover">Rover</Link><br />
+      <Link href="/search">Media Search</Link><br />
+      <Link href="/asteroids">Daily Asteroids</Link><br />
+      <Link href="/patent">Patents Search</Link><br />
+      <Link href="/projects">Projects Search</Link><br />
       
     </div>
   );
